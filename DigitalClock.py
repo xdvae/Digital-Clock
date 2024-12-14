81687a58c6f2ac658ae750a45b49aa1d54db8ced
vaibhav <vaibhavbusiness2005@gmail.com>
2024-12-15 01:58:04 +0530

    Unfinished Python Digital Clock v1.1 Updated: New Weather API, New Live weather features (work in progress).

--- DigitalClock.py
+++ DigitalClock.py
@@ -1,6 +1,7 @@
 from flask import Flask ,render_template, jsonify,request
 import requests
 from datetime import datetime
+import time
 import pytz
 
 app = Flask(__name__)
@@ -9,55 +10,96 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
      return render_template('home.html')
-
+dev_mode = 0 # Dev mode.
 old_city = 0
 old_zone = "Local"
+city = 0
+weather = None
+@app.route("/get_timzeone_via_ip", methods=['POST'])
+def get_timezone_via_IP():
+    data = request.get_json()
+    my_ip =  data.get('IP')
+    print("Function called\nIP: ",my_ip)
+    url = f"http://ip-api.com/json/{my_ip}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        global city
+        data = response.json()
+        city = data['city']
+        print("City: ",city)
+
+        formatted_time = {
+            "city_name":city
+        }
+        return formatted_time
+    
 
 @app.route('/get_city_time', methods=['POST'])
 def get_city_time():
     data = request.get_json()
     city_name = str(data.get('City_name'))
-    city_name = city_name.lower()
-    city_name = city_name.capitalize()
-    city_timezone = get_timezone(city_name)
-    timezone = pytz.timezone(city_timezone)
-    print(city_timezone)
-    now = datetime.now(timezone)
-    # print(now.hour)
+    # print("City Name (GCT): ",city_name)
+    # I'll Label this because its a mess below.
+    # The city name gets sent to the function GET_TIMEZONE() which returns its timezone name('Asia/Tokyo).
+    timezone_object = pytz.timezone(get_timezone(city_name))
+    # print("\n\nTimezone Object (GCT): ",timezone_object)
+    # This gets the current time from the timezone onject and returns it.
+    now = datetime.now(timezone_object)
+    # print("Time now (GCT): ",now)
+    # print("Hour now (GCT): ",now.strftime("%I"))
+    # print("2nd: ",weather)
     formatted_time = {
         "hour": now.strftime("%I"),
         "min": now.strftime("%M"),
         "sec": now.strftime("%S"),
         "am_pm": now.strftime("%p"),
-        "current_timezone": city_timezone,
-        "background_selector" : background_selector(now.hour)
+        "current_timezone": get_timezone(city_name),
+        "background_selector" : background_selector(now.hour),
+        "city":city,
+        "condition":weather['current']['condition']['text'],
+        "temp":weather['current']['temp_c'],
+        "temp_h":weather['forecast']['forecastday'][0]['day']['maxtemp_c'],
+        "temp_l":weather['forecast']['forecastday'][0]['day']['mintemp_c']
+        #Currently at temp low. TODO: 15/12/24: Make a feels like indicator, fill all the holders with data and Make the auto div generation possible using foreach. 
     }
+    
     return formatted_time
 
 def get_timezone(city_name):
     global old_city
     global old_zone
+    global weather
     if(city_name != old_city):
+        print("Sent New GET_TIMEZONE request to API.")
         old_city = city_name
-        url = f"https://api.opencagedata.com/geocode/v1/json?q=Rua+Cafel%C3%A2ndia%2C+Carapicu%C3%ADba%2C+{city_name}&key=a3556c787c6a451cb1e5507351caf9fb&pretty=1"
+        # DEVELOPMENT MODE: set the dev_mode var to 0 to work with live cities.
+        if(dev_mode == 0):
+            # url_1 = f"https://api.opencagedata.com/geocode/v1/json?q=Rua+Cafel%C3%A2ndia%2C+Carapicu%C3%ADba%2C+{city_name}&key=a3556c787c6a451cb1e5507351caf9fb&pretty=1"
 
-        response = requests.get(url)
+            url_2 = f"https://api.weatherapi.com/v1/c/forecast.json?key=fdf65fe5213f4e5287d165833241212&q={city_name}"
+            response = requests.get(url_2)
+            if response.status_code == 200:
 
-        if response.status_code == 200:
-            data = response.json()  
-
-            results = data['results'][0]['annotations']['timezone']['name']
-            old_zone = results
-            
-            return results
+                weather = response.json()  
+                # results = data['results'][0]['annotations']['timezone']['name']
+                results = weather['location']['tz_id']
+                # print(weather)
+                old_zone = results
+                print("results: ",results)
+                return results
+            else:
+                return "UTC"
         else:
-            return "UTC"
+            print("In DEV MODE: no API used.")
+            old_zone = "Asia/Kolkata"
+            return "Asia/Kolkata"
     else:
-        print("returned old")
+        # print("Returned the saved timezone value.")
+
         return old_zone
 
 def background_selector(hour):
-    
     if(hour >= 5 and hour < 10):
         return "sunrise"
     elif(hour >= 10 and hour < 16):
@@ -67,25 +109,12 @@ def background_selector(hour):
     elif(hour >= 19 and hour <= 24) or (hour < 5):
         return "night"
 
-@app.route('/gettime')
-def gettime():
-    # time.sleep(1)
-    now = datetime.now()
-    print(now.hour)
-    formatted_time = {
-        "hour": now.strftime("%I"),
-        "min": now.strftime("%M"),
-        "sec": now.strftime("%S"),
-        "am_pm": now.strftime("%p"),
-        "current_timezone": "Local",
-        "background_selector" : background_selector(now.hour)
-    }
-
-    return jsonify(formatted_time)
-    
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
 
 
-#TODO: 9/12/24: Rewrite the API code and test it's response in the scratch file before applying in the main file. 
+#TODO: 9/12/24: Rewrite the API code and test it's response in the scratch file before applying in the main file. DONE
+
+#TODO: 13/12/24: When the website is loaded the Current timezone gets set to: Sao Palao, Find and fix the bug. DONE
+
+#TODO: 13/12/24: After fixing The bug above. USE Weather API's api to show weather data. ADD another section for weather. 
